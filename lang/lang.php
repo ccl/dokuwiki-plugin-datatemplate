2130972f41045a7050c6d2c637a272820329acfe
Juergen-aus-Koeln <hans-juergen.schuemmer@web.de>
2017-09-12 10:29:07 +0200

    file for german language

---
+++ lang/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$lang['nextpage'] = 'Nächste Seite';
+$lang['prevpage'] = 'Vorherige Seite';
+
+// not possible
+//$lang['filterany'] = 'Alle';
+$lang['searchfilter'] = 'Suche/Filter';
+$lang['submitfilter'] = 'Filter hinzufügen';
+$lang['prevfilters'] = 'Aktive Filter';
